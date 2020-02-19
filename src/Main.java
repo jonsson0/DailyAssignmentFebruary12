0412de85acef69d2182f577b3fddeff750e2cdbc
Mads Jönsson <Madsjonsson1997@gmail.com>
2020-02-20 00:11:34 +0100

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(add(8,8));
+
+
+    }
+
+    public static int add(int a, int b) {
+        return a+b;
+
+    }
+
+
+}
